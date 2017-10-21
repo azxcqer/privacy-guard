488854cce5e344b2ea5327096a57edf5496d2d01
Natan Carvalho <natanbcpc@gmail.com>
2017-10-21 19:33:13 -0200

    Tecla esc sai do jogo

--- scripts/global.gd
+++ scripts/global.gd
@@ -6,7 +6,13 @@ var resize = 3
 
 func _ready():
 	get_big()
+	set_process_input(true)
 
 func get_big():
 	if os != "Html5":
 		OS.set_window_size(resize * size)
+
+func _input(event):
+	if event.type == InputEvent.KEY:
+		if Input.is_key_pressed(KEY_ESCAPE):
+			get_tree().quit()
