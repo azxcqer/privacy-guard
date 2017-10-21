b2c407a0b196f2af80c78e26cc2918428c4de946
André Andrade <ardy.aileron@gmail.com>
2017-10-21 04:31:05 -0200

    arrumado resize?
    eu acho?

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,3 +1,14 @@
 extends Node
 
+var os = OS.get_name()
+var size = OS.get_window_size()
+
+func _ready():
+	get_big()
+	pass
+
+func get_big():
+  if os != "Html5":
+    OS.set_window_size(8 * size)
+
 var grid_dimension = Vector2(Globals.get("display/height"), Globals.get("display/height"))
