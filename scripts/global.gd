9638686e75235235188595cca0f86b23ea0f7de3
André Andrade <ardy.aileron@gmail.com>
2017-10-21 11:09:32 -0200

    resize stuff

--- scripts/global.gd
+++ scripts/global.gd
@@ -2,10 +2,11 @@ extends Control
 
 var os = OS.get_name()
 var size = OS.get_window_size()
+var resize = 3
 
 func _ready():
 	get_big()
 
 func get_big():
 	if os != "Html5":
-		OS.set_window_size(8 * size)
+		OS.set_window_size(resize * size)
