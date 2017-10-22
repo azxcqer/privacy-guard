740333a89503322486ec4958756e1dd8524b4513
André Andrade <ardy.aileron@gmail.com>
2017-10-22 03:16:36 -0200

    sprite changing animation

--- scripts/moves.gd
+++ scripts/moves.gd
@@ -1,7 +1,8 @@
 extends RigidBody2D
 
 # move vars
-export var dir = -1
+export var dir = 1
+var dirLast = dir
 var autoMoves = true
 var speed = 40
 var jumpHeight = -100
@@ -14,6 +15,7 @@ func _ready():
 	ray.add_exception(self)
 	set_fixed_process(true)
 	set_process(true)
+	change_direction()
 	
 func die():
 	queue_free()
@@ -21,11 +23,18 @@ func die():
 func jump():
 	set_axis_velocity(Vector2(0,jumpHeight))
 	jump = false
+
+func change_direction():
+	if dir == 1:
+		set_scale(Vector2(1,1))
+	if dir == -1:
+		set_scale(Vector2(-1,1))
 	
 func _process(delta):
 	var a = ray.is_colliding()
 	if a && get_linear_velocity().y <= 0 && jumper:
 		jump = true
+	
 
 func _fixed_process(delta):
 	# keep me straight as not to fuck everything up
@@ -36,6 +45,8 @@ func _fixed_process(delta):
 	# if I can move so I will
 	if dir != 0:
 		set_axis_velocity(Vector2(speed * dir, 0))
+	
+	change_direction()
 
 	if autoMoves:
 		pass
