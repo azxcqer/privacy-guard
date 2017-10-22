8b64450a94129d3fedb42e684594702b9ae8f7c6
André Andrade <ardy.aileron@gmail.com>
2017-10-22 12:10:32 -0200

    file thingie
    and terminal level changing

---
+++ scripts/file.gd
@@ -0,0 +1,21 @@
+extends "res://scripts/pickup.gd"
+
+onready var zone = get_node("Area2D")
+export var nextLevel = ""
+
+func _ready():
+	zone.connect("area_enter", self, "collides")
+
+func destroy():
+	queue_free()
+
+func collides(thing):
+	var a = thing.get_parent()
+	print(a)
+	if "isBad" in a:
+		destroy()
+	if "isTerminal" in a:
+		if a.isTerminal:
+			print("sucess!")
+			print(nextLevel)
+			global.next(nextLevel)
